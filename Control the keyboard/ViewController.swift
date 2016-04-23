857fa477514b87b64ff712cf91e0003d7de49b0f
Mohak <mohakagr@gmail.com>
2016-04-23 23:40:50 +0530

    Keyboard behaviour changed on return, touching anywhere outside the keyboaard

--- Control the keyboard/ViewController.swift
+++ Control the keyboard/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var textInput: UITextField!
+    @IBOutlet weak var labelText: UILabel!
+    @IBAction func onButtonClick(sender: AnyObject) {
+        labelText.text = textInput.text
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.textInput.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true);
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
 }
 
